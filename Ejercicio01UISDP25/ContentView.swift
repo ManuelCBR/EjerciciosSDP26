09f38ccd86fc53842900043f9fd0a39103bef99a
ManuelCBR <manuelvs1987@gmail.com>
2025-11-15 08:02:30 +0100

    Initial Commit

---
+++ Ejercicio01UISDP25/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ejercicio01UISDP25
+//
+//  Created by Manuel Bermudo on 15/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ejercicio01UISDP25/Ejercicio01UISDP25App.swift
@@ -0,0 +1,17 @@
+//
+//  Ejercicio01UISDP25App.swift
+//  Ejercicio01UISDP25
+//
+//  Created by Manuel Bermudo on 15/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Ejercicio01UISDP25App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
